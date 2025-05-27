e61238264da174cf3ca8268f75e270653f285573
Kevinmonitor <trantheminh@gmail.com>
2025-05-27 10:17:20 +1000

    hotfix for water tile rounding

--- Maps/WaterHandling.gd
+++ Maps/WaterHandling.gd
@@ -7,7 +7,7 @@ func _physics_process(delta: float) -> void:
 	
 	MainGame.get_singleton().playerPath.underwater = false
 	# check if player is in water
-	playerCoords = local_to_map(MainGame.get_singleton().playerPath.position)
+	playerCoords = local_to_map(to_local(Vector2(MainGame.get_singleton().playerPath.WaterCollisionArea.global_position.x, MainGame.get_singleton().playerPath.WaterCollisionArea.global_position.y)))
 	tile = get_cell_tile_data(playerCoords)
 	if tile != null:
 		if tile.get_custom_data("water") == true:
